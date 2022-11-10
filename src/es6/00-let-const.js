e3b3ae4a25b0870695e3e5ebe5cf79bf1485188e
nestorrg18 <neriga10@hotmail.com>
2022-11-09 20:28:19 -0600

    Clase 4:let y const, y arrow functions

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,24 @@
+var lastName = "Rios";
+lastName = "Garcia";
+console.log(lastName);
+//
+let fruit = "Apple";
+fruit = "Kiwi";
+console.log(fruit);
+//
+const animal = "dog";
+animal = "cat";
+console.log(animal);
+//
+//
+const fruits = () => {
+    if (true) {
+        var fruit1 = "Apple"; //function scope
+        let fruit2 = "Kiwi"; //block scope
+        const fruit3 = "Banana"; //block scope
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+fruits(); 
