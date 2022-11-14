38c2e518d19357375488ea5a295858d67bc61624
nestorrg18 <neriga10@hotmail.com>
2022-11-14 16:57:56 -0600

    Clase 5; strings

---
+++ src/es6/03-strings.js
@@ -0,0 +1,18 @@
+let hello = "Hello";
+let world = "Word";
+let epicPhrase = hello +" "+world+"!";
+console.log(epicPhrase);
+
+//Template Lietrals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//Multi-line strings
+let lorem = "Esto es un string \n " + "Esto es otra linea";
+
+let lorem2 = `Esta es una frase epica
+    Esta es la continuacion de la frase.
+`;
+
+console.log(lorem);
+console.log(lorem2);
