7e63e2223ad0f963f2160630a70d181d597643b2
nestorrg18 <neriga10@hotmail.com>
2022-11-15 20:47:49 -0600

    Clase 8; spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -11,3 +11,28 @@ let user = {username: "Oscar", age: 36};
 let {username, age} = user;
 console.log(username, age);
 console.log(username, user.age);
+
+// spread operator
+
+let person = {name: "Nestor", age: 20};
+let country = "CA";
+
+let data = {id:1,...person, country};
+console.log (data);
+
+// rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);
+
+//
+let kissEmoji = [..."👩‍❤️‍💋‍👩"]
+console.log(kissEmoji)
+
+let familyEmoji = [..."👨‍👩‍👦‍👦"]
+console.log(familyEmoji)
