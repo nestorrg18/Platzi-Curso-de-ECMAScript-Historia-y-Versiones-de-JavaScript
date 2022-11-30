7276c16647c67cb451e1c915a4fd8445847700b5
nestorrg18 <neriga10@hotmail.com>
2022-11-30 17:49:20 -0600

    Clase 13:Generator

---
+++ src/es6/09-generator.js
@@ -0,0 +1,21 @@
+function* iterate(array) {
+    for (let value of array){
+        yield value;
+    }
+}
+
+const it = iterate(["Juan", "Mateo", "Lucas", "Pedro"])
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+
+// Los generadores son funciones especiales que pueden pausar su ejecución, 
+// luego volver al punto donde se quedaron, recordando su scope y seguir retornando valores.
+
+// Estos se utilizan para guardar la totalidad de datos infinitos,
+// a través de una función matemática a valores futuros. De esta manera ocupan poca memoria, 
+// con respecto a si creamos un array u objeto.
